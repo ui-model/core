788990d412bb736105e522b26e54673ce7e60322
Zhicheng WANG <zcwang@thoughtworks.com>
2019-09-15 22:57:02 +0800

    feat: 计算距离的长度

--- src/lib/utils/distance.ts
+++ src/lib/utils/distance.ts
@@ -2,6 +2,10 @@ export class Distance {
   constructor(public x: number = 0, public y: number = 0) {
   }
 
+  getLength(): number {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
   zoom(scale: number): void {
     this.x *= scale;
     this.y *= scale;
